295eed3b54eccd443800398428e1539386235292
MightyKnightmare <sajad.var83@gmail.com>
2023-06-24 11:31:46 +0330

    created an fxml and controller file for Passenger Flights List (Buying Tickets): -PassengerFlights.fxml -PassengerFlights.java

---
+++ src/main/java/com/example/demo1/Controller/PassengerFlights.java
@@ -0,0 +1,116 @@
+package com.example.demo1.Controller;
+import Model.Airport.Flight;
+import com.example.demo1.Main;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PassengerFlights implements Initializable
+{
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button buyButton;
+    @FXML
+    private Label checker;
+    @FXML
+    private Label purchaseLabel;
+    @FXML
+    private ListView<String> dateList;
+    @FXML
+    private ListView<String> fromList;
+    @FXML
+    private ListView<String> idList;
+    @FXML
+    private TextField inputTicketNum;
+    @FXML
+    private ListView<String> lengthList;
+    @FXML
+    private ListView<String> numList;
+    @FXML
+    private ListView<String> priceList;
+    @FXML
+    private ListView<String> ticketsList;
+    @FXML
+    private ListView<String> timeList;
+    @FXML
+    private ListView<String> toList;
+
+    protected int selectedFlight;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buyButton.setDisable(true);
+        inputTicketNum.setDisable(true);
+        purchaseLabel.setVisible(false);
+        updateLists(numList, idList, fromList, toList, dateList, timeList, lengthList, ticketsList, priceList);
+
+        numList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                inputTicketNum.setDisable(false);
+                selectedFlight = numList.getSelectionModel().getSelectedIndex();
+            }
+        });
+        inputTicketNum.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> ov, String t, String t1) {
+                if(!(t1.equals("")))
+                    buyButton.setDisable(false);
+            }
+        });
+    }
+
+    @FXML
+    void pressedBuy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressedBack(ActionEvent event) throws IOException {
+        Main.sceneSwitch("PassengerPage.fxml", event, 520, 400);
+    }
+
+    protected static void updateLists(ListView<String> numList, ListView<String> idList, ListView<String> fromList, ListView<String> toList, ListView<String> dateList, ListView<String> timeList, ListView<String> lengthList, ListView<String> ticketsList, ListView<String> priceList) {
+        try {
+            numList.getItems().clear();
+            idList.getItems().clear();
+            fromList.getItems().clear();
+            toList.getItems().clear();
+            dateList.getItems().clear();
+            timeList.getItems().clear();
+            lengthList.getItems().clear();
+            ticketsList.getItems().clear();
+            priceList.getItems().clear();
+
+            for (Flight obj : Main.allFlights) {
+                int seats = obj.getPlane().getSeats();
+                int soldTickets = obj.getSoldTickets();
+                if(seats-soldTickets == 0)
+                    continue;
+                if(!(obj.getStatus() == Flight.Status.OPEN))
+                    continue;
+
+                numList.getItems().add(Integer.toString(Main.allFlights.indexOf(obj)+1));
+                idList.getItems().add(obj.getId()+"");
+                fromList.getItems().add(obj.getFrom());
+                toList.getItems().add(obj.getTo());
+                dateList.getItems().add(obj.getMonth()+"/"+obj.getDay());
+                timeList.getItems().add(obj.getHour()+":"+obj.getMinute());
+                lengthList.getItems().add(obj.getFlightLenght()+"H");
+                priceList.getItems().add(obj.getTicket().getPrice()+"$");
+                ticketsList.getItems().add((seats-soldTickets)+"");
+            }
+        } catch(Exception e) { Main.appendToFile(e); }
+    }
+}
